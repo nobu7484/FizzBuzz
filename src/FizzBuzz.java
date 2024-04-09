960898413832ce2d6895c233bd01126de13778ff
yukinobu.ueno <Owner@DESKTOP-LUV71M1>
2024-04-09 23:26:47 +0900

    課題再提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		for (int a=1;a<=100;a++) {
-			System.out.println(a);
+			
 			int val=(a);
 			if (a%3<=0&&a%5<=0) {
 				System.out.println("FizzBuzz");
@@ -11,6 +11,8 @@ public class FizzBuzz {
 				System.out.println("Fizz");
 			}else if(a%5==0) {
 				System.out.println("Buzz");
+			}else {
+				System.out.println(a);
 			}
 	}
 }
